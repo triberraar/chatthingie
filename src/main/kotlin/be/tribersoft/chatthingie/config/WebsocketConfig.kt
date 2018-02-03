e251aa65dfb14170e7cc382a40bdb20102a31205
geert olaerts <geertolaerts@geerts-MBP.lan>
2018-02-03 23:56:27 +0100

    made cross origin dependend on dev profile

--- src/main/kotlin/be/tribersoft/chatthingie/config/WebsocketConfig.kt
+++ src/main/kotlin/be/tribersoft/chatthingie/config/WebsocketConfig.kt
@@ -2,15 +2,17 @@ package be.tribersoft.chatthingie.config
 
 import be.tribersoft.chatthingie.ws.ChatHandler
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @EnableWebSocket
-class WebsocketConfig(private val chatHandler: ChatHandler): WebSocketConfigurer {
+class WebsocketConfig(private val chatHandler: ChatHandler, private val environment: Environment): WebSocketConfigurer {
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        registry.addHandler(chatHandler, "/chat").setAllowedOrigins("*")
+      val registration = registry.addHandler(chatHandler, "/chat")
+      if(environment.activeProfiles.contains("dev")) registration.setAllowedOrigins("*")
     }
 
 }
